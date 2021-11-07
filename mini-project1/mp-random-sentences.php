b8c0e2f112a5ac34a05f7e9421fdc7fc7947b10b
ali rakhshanipour <ali.rakhshanipour.sru@gmail.com>
2021-11-07 23:42:18 +0330

    mini project added

---
+++ mini-project1/mp-random-sentences.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Random Sentences</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center lead text-capitalize my-4">random sentences</h1>
+        <div class="row justify-content-center rounded border">
+            <div class="rSentence">
+                Lorem ipsum dolor sit amet consectetur.<br>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        setInterval(function() {
+            $.ajax({
+                url: 'ajax-rSentenceGenerator.php',
+                method: 'POST',
+                data: {
+                    msg: "hi server"
+                },
+                success: function(response) {
+                    resultTag.html(response);
+                },
+                error: function(response) {
+                    resultTag.html(response);
+                }
+            });
+        }, 2000);
+    </script>
+</body>
+
+</html>
